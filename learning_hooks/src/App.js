baafd801328f32a91f4172c248f20945583da084
Gawsan <gawsan98@gmail.com>
2022-10-12 10:19:00 +0530

    updated file

--- learning_hooks/src/App.js
+++ learning_hooks/src/App.js
@@ -1,5 +1,11 @@
+import UseStateIdea from "./Component/UseStateIdea";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <UseStateIdea />
+    </div>
+  );
 }
 
 export default App;
